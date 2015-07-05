aef035fbdf3263a1ce816c6b413af413a0e0e079
Jason Kusnier <jkusnier@gmail.com>
2015-07-05 11:37:19 -0400

    removing messy comments

--- WorkoutMerge/SubmitWorkoutTableViewCell.swift
+++ WorkoutMerge/SubmitWorkoutTableViewCell.swift
@@ -19,13 +19,10 @@ class SubmitWorkoutTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
     @IBAction func switchChanged(sender: AnyObject) {
--- WorkoutMerge/ViewController.swift
+++ WorkoutMerge/ViewController.swift
@@ -69,7 +69,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
