8aee15e1a131dd0f14d9bd8bc30a254cbe852d25
Jason Kusnier <jkusnier@gmail.com>
2015-08-01 16:14:30 -0400

    started protocol definition

--- WorkoutMerge/WorkoutSyncAPI.swift
+++ WorkoutMerge/WorkoutSyncAPI.swift
@@ -7,7 +7,19 @@
 //
 
 import UIKit
+import HealthKit
 
 protocol WorkoutSyncAPI {
     
+    func authorizeEmbeddedFrom(controller: UIViewController, params: [String : String]?, afterAuthorizeOrFailure: (wasFailure: Bool, error: NSError?) -> Void)
+    
+    func disconnect()
+    
+//    class func handleRedirectURL(url: NSURL)
+    
+    func postActivity(workout: (UUID: NSUUID?, type: String?, startTime: NSDate?, totalDistance: Double?, duration: Double?, averageHeartRate: Int?, totalCalories: Double?, notes: String?, otherType: String?), failure fail : ((NSError?, String) -> ())?, success succeed: ((savedKey: String?) -> ())?)
+    
+    static func activityType(t: HKWorkoutActivityType) -> String
+    
+    static func otherActivityType(t: HKWorkoutActivityType) -> String?
 }
