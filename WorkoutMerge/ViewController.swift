86abf9a9bef32452d2c6e4c4c73e3161e8d3c995
Jason Kusnier <jkusnier@gmail.com>
2015-10-03 15:37:11 -0400

    re-enabled sync all button

--- WorkoutMerge/ViewController.swift
+++ WorkoutMerge/ViewController.swift
@@ -13,7 +13,7 @@ import CoreData
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-//    @IBOutlet weak var syncAllButton: UIBarButtonItem!
+    @IBOutlet weak var syncAllButton: UIBarButtonItem!
     
     let hkStore = HKHealthStore()
     var workouts = [HKWorkout]()
@@ -49,7 +49,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         if let linkedServices = self.defaults.arrayForKey("linkedServices") as? [String] {
             self.linkedServices = linkedServices.sort() {$0 < $1}
-//            self.syncAllButton.enabled = linkedServices.count > 0
+            self.syncAllButton.enabled = linkedServices.count > 0
         }
         
         if !HKHealthStore.isHealthDataAvailable() {
@@ -98,7 +98,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         if !self.initialAppearance {
             if let linkedServices = self.defaults.arrayForKey("linkedServices") as? [String] {
                 self.linkedServices = linkedServices.sort() {$0 < $1}
-//                self.syncAllButton.enabled = linkedServices.count > 0
+                self.syncAllButton.enabled = linkedServices.count > 0
             }
             
             // Refresh for tableview accessories
